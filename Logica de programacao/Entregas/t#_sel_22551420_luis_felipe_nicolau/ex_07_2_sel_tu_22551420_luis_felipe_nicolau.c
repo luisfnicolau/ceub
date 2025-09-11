18fe78dcfee6d87fd52f5b639dda7711a83f953d
Luis Felipe Nicolau <luisfnicolau@dinfor.uniceub>
2025-09-11 10:46:15 -0300

    Lista 2 - exercicio 7

--- Logica de programacao/Entregas/t#_sel_22551420_luis_felipe_nicolau/ex_07_2_sel_tu_22551420_luis_felipe_nicolau.c
+++ Logica de programacao/Entregas/t#_sel_22551420_luis_felipe_nicolau/ex_07_2_sel_tu_22551420_luis_felipe_nicolau.c
@@ -9,4 +9,29 @@
 
 void main(void) {
     setlocale(LC_ALL, "Portuguese");
+    
+    float nota1, nota2, media;
+    int peso1, peso2;
+    
+    printf("\nPrimeira nota: ");
+    scanf("%f", &nota1);
+    
+    printf("\nPeso da primeira nota: ");
+    scanf("%i", &peso1);
+    
+    printf("\nSegunda nota: ");
+    scanf("%f", &nota2);
+    
+    printf("\nPeso da segunda nota: ");
+    scanf("%i", &peso2);
+    
+    media = ((nota1 * peso1) + (nota2 * peso2)) / (peso1 + peso2);
+    
+    printf("\nMedia: %.2f ", media);
+    
+    if (media >=5) {
+    	printf("Aluno aprovado");
+	} else {
+		printf("Aluno reprovado");
+	}
 }
