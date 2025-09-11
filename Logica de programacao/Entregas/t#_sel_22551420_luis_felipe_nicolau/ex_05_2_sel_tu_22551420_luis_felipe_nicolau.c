25c600e9dc4efa9d00d38107a4c205cfe56aa096
Luis Felipe Nicolau <luisfnicolau@dinfor.uniceub>
2025-09-11 09:20:27 -0300

    Lista 2 - Exercicio 5

--- Logica de programacao/Entregas/t#_sel_22551420_luis_felipe_nicolau/ex_05_2_sel_tu_22551420_luis_felipe_nicolau.c
+++ Logica de programacao/Entregas/t#_sel_22551420_luis_felipe_nicolau/ex_05_2_sel_tu_22551420_luis_felipe_nicolau.c
@@ -1,5 +1,5 @@
 /*
- * DescriÃ§Ã£o: Verifica se um numero Ã© par ou impar.
+ * Descrição: Verifica se um numero é par ou impar.
  * Autor: Luis Felipe Nicolau
  * Data: 11/09/2025
  */
@@ -9,4 +9,16 @@
 
 void main(void) {
     setlocale(LC_ALL, "Portuguese");
+    
+    int valor;
+    
+    printf("Entre com o número: ", valor);
+    
+    scanf("%i", &valor);
+    
+    if (valor % 2 == 0) {
+    	printf("Este é um número par");
+	} else {
+		printf("Este é um número impar");
+	}
 }
