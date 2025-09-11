96caf72b5fadcfac924f89af7396121e6d4e686b
Luis Felipe Nicolau <luisfnicolau@dinfor.uniceub>
2025-09-11 09:28:39 -0300

    Lista 2 - exercicio 6

--- Logica de programacao/Entregas/t#_sel_22551420_luis_felipe_nicolau/ex_06_2_sel_tu_22551420_luis_felipe_nicolau.c
+++ Logica de programacao/Entregas/t#_sel_22551420_luis_felipe_nicolau/ex_06_2_sel_tu_22551420_luis_felipe_nicolau.c
@@ -9,4 +9,22 @@
 
 void main(void) {
     setlocale(LC_ALL, "Portuguese");
+    float valorCompra, valorVenda;
+    
+    printf("\nValor da compra: ");
+    scanf("%f", &valorCompra);
+    
+    printf("\nValor da venda: ");
+    scanf("%f", &valorVenda);
+    
+    printf("\nValor da compra: %.2f", valorCompra);
+    printf("\nValor da venda: %.2f", valorVenda);
+    
+    if (valorCompra > valorVenda) {
+    	printf("\nTeve prejuízo");
+	} else if (valorCompra < valorVenda) {
+		printf("\nTeve lucro");
+	} else {
+		printf("\nOs valores são iguais");
+	}
 }
