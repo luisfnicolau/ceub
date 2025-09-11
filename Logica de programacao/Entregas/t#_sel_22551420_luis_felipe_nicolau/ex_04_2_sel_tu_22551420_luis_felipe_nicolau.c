6d01fae2f7b26474ca8a338a07c7282e57d3b29c
Luis Felipe Nicolau <luisfnicolau@dinfor.uniceub>
2025-09-11 09:12:50 -0300

    Lista 2 - exercicio 4

--- Logica de programacao/Entregas/t#_sel_22551420_luis_felipe_nicolau/ex_04_2_sel_tu_22551420_luis_felipe_nicolau.c
+++ Logica de programacao/Entregas/t#_sel_22551420_luis_felipe_nicolau/ex_04_2_sel_tu_22551420_luis_felipe_nicolau.c
@@ -9,4 +9,23 @@
 
 void main(void) {
     setlocale(LC_ALL, "Portuguese");
+
+    char sex;
+    float altura, pesoIdeal;
+    printf("\nO peso a ser calculado é de um homem ou de uma mulher? (Homem: m, Mulher: f): ");
+    scanf("%c", &sex);
+
+    if (sex != 'm' && sex != 'f'){
+        printf("Gênero inválido: %c", sex);
+    }
+    printf("\nEntre com a altura(m): ");
+    scanf("%f", &altura);
+
+    if (sex == 'm') {
+        pesoIdeal = (72.7 * altura) - 58;
+    } else {
+        pesoIdeal = (62.1 * altura) - 44.7;
+    }
+
+    printf("\nO peso ideial é: %.2f", pesoIdeal);
 }
