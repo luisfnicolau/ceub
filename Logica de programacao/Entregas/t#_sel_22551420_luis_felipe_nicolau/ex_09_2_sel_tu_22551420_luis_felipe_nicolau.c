3a65ab5a10c82666a4b98872bf9419b4cf03527f
Luis Felipe Nicolau <luisfnicolau@dinfor.uniceub>
2025-09-11 11:17:39 -0300

    Lista 2 - exercicio 9

--- Logica de programacao/Entregas/t#_sel_22551420_luis_felipe_nicolau/ex_09_2_sel_tu_22551420_luis_felipe_nicolau.c
+++ Logica de programacao/Entregas/t#_sel_22551420_luis_felipe_nicolau/ex_09_2_sel_tu_22551420_luis_felipe_nicolau.c
@@ -6,7 +6,25 @@
 
 #include <stdio.h>
 #include <locale.h>
+#include <math.h>
 
 void main(void) {
     setlocale(LC_ALL, "Portuguese");
+    
+    float x1, x2, delta;
+    int a, b, c;
+    
+    
+    printf("\nEntre com o valor do coeficiente a: ");
+    scanf("%i", &a);
+    printf("\nEntre com o valor do coeficiente b: ");
+    scanf("%i", &b);
+    printf("\nEntre com o valor do coeficiente c: ");
+    scanf("%i", &c);
+        
+    delta = pow(b, 2) - (4*a*c);
+    x1 = (-b + sqrt(delta)) / (2 * a);
+	x2 = (-b - sqrt(delta)) / (2 * a);    
+    
+    printf("\nAs raizes são: %.4f e %.4f", x1, x2);
 }
